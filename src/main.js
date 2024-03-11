09101a3e7b0e56e623caa2beacd5e31e8cf98f46
Adrien Husson <husson.adr@gmail.com>
2024-03-11 16:46:08 +0100

    transforms

--- src/main.js
+++ src/main.js
@@ -1,53 +1,51 @@
-console.log("Hello Javascript")
+const canvas = document.querySelector('canvas')
+const ctx = canvas.getContext('2d') // context type: https://developer.mozilla.org/fr/docs/Web/API/HTMLCanvasElement/getContext#typedecontexte
 
-let canvas = document.querySelector('canvas')
-let ctx = canvas.getContext('2d') // context type: https://developer.mozilla.org/fr/docs/Web/API/HTMLCanvasElement/getContext#typedecontexte
+const size = 100
+const red =  '#E83A4E'
+const yellow = '#FFE800'
+const blue = '#3B76F5'
+const green = '#71E394'
 
-// 1 - style principle and shape shortcut
-ctx.fillStyle = 'blue'
-ctx.strokeStyle = "red"
-ctx.lineWidth = 4
-ctx.fillRect(100, 100, 100, 100)
-ctx.strokeRect(100, 100, 100, 100)
+const drawRectangle = (color) => {
+    ctx.fillStyle = color
+    ctx.beginPath()
+    // ctx.rect(0, 0, size, size)
+    ctx.rect(-size/2, -size/2, size, size) // centrage de la géométrie
+    ctx.fill()
+    ctx.closePath()
+}
 
-// 2 - rect path
-ctx.fillStyle = '#ff0000'
-ctx.strokeStyle = "#0000ff"
-ctx.beginPath()
-ctx.rect(300, 100, 100, 100) // order !!
-ctx.fill() // order !!
-ctx.stroke() // order !!
-ctx.closePath()
-
-// 3 - circle + arc path
 const deg2rad = (deg) => {
     return deg * 2 * Math.PI / 360
 }
-ctx.strokeStyle = "#0000ff"
-ctx.lineWidth = 10
-ctx.lineCap = "round" // https://developer.mozilla.org/fr/docs/Web/API/CanvasRenderingContext2D/lineCap
-ctx.beginPath()
-ctx.arc(150, 350, 50, 0, deg2rad(180))
-ctx.stroke()
-ctx.closePath()
 
-// 4 - free path (lines)
-ctx.beginPath()
-ctx.moveTo(300, 300)
-ctx.lineTo(400, 400)
-ctx.lineTo(300, 400)
-ctx.lineTo(400, 300)
-ctx.lineTo(300, 300)
-ctx.lineJoin = "round" // https://developer.mozilla.org/fr/docs/Web/API/CanvasRenderingContext2D/lineJoin
-ctx.stroke()
-ctx.closePath()
+// 0 - Graphique offset
+ctx.translate(size / 2, size / 2)
 
-// 5 - free path (beziers)
-ctx.beginPath()
-ctx.moveTo(100, 500)
-ctx.bezierCurveTo(100, 550, 200, 450, 200, 500) // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/bezierCurveTo
-ctx.bezierCurveTo(150, 500, 250, 600, 200, 600)
-ctx.bezierCurveTo(200, 550, 100, 650, 100, 600)
-ctx.bezierCurveTo(150, 600, 50, 500, 100, 500)
-ctx.stroke()
-ctx.closePath()
+// 1 - Translate
+ctx.translate(100, 100)
+drawRectangle(blue)
+ctx.translate(-100, -100) // ! translate ne prend pas de valeurs absolues : il faut retrancher par rapport à la position existante
+drawRectangle(yellow)
+
+// 2 - Rotate
+ctx.translate(200, 200)
+ctx.rotate(deg2rad(45)) // !! order
+drawRectangle(red)
+
+// ------------------
+ctx.resetTransform() // reset transform
+// ------------------
+
+// 3 - Save & Restore
+ctx.save()
+ctx.translate(300, 0)
+drawRectangle(green)
+ctx.restore()
+
+ctx.save()
+ctx.translate(300, 300)
+ctx.rotate(deg2rad(45)) // !! order
+drawRectangle(green)
+ctx.restore()
