1a3132627ad06837ce0742d645cb503151ca4899
Adrien Husson <husson.adr@gmail.com>
2024-03-12 16:30:56 +0100

    init

--- src/main.js
+++ src/main.js
@@ -1,17 +1,26 @@
 const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d') // context type: https://developer.mozilla.org/fr/docs/Web/API/HTMLCanvasElement/getContext#typedecontexte
 
-const size = 100
-const red =  '#E83A4E'
+/**
+ *  CONFIG
+ */
+
+const ROWS = 6
+const COLS = 6
+
+const red = '#E83A4E'
 const yellow = '#FFE800'
 const blue = '#3B76F5'
 const green = '#71E394'
+const colors = [red, yellow, blue, green]
 
-const drawRectangle = (color) => {
+/**
+ *  METHODS
+ */
+const drawRectangle = (cWidth, cHeight, color) => {
     ctx.fillStyle = color
     ctx.beginPath()
-    // ctx.rect(0, 0, size, size)
-    ctx.rect(-size/2, -size/2, size, size) // centrage de la géométrie
+    ctx.rect(-cWidth / 2, -cHeight / 2, cWidth, cHeight)
     ctx.fill()
     ctx.closePath()
 }
@@ -20,32 +29,35 @@ const deg2rad = (deg) => {
     return deg * 2 * Math.PI / 360
 }
 
-// 0 - Graphique offset
-ctx.translate(size / 2, size / 2)
+/**
+ *  MAIN
+ */
 
-// 1 - Translate
-ctx.translate(200, 200)
-drawRectangle(blue)
-ctx.translate(-200, -200) // ! translate ne prend pas de valeurs absolues : il faut retrancher par rapport à la position existante
-drawRectangle(yellow)
+// Get canvas dimensions
+console.log(canvas.getBoundingClientRect())
+const canvasWidth = canvas.getBoundingClientRect().width
+const canvasHeight = canvas.getBoundingClientRect().height
+canvas.width = canvasWidth
+canvas.height = canvasHeight
 
-// 2 - Rotate
-ctx.translate(400, 400)
-ctx.rotate(deg2rad(45)) // !! order
-drawRectangle(red)
+// Compute cells dimensions
+const cellWidth = canvasWidth / COLS
+const cellHeight = canvasHeight / ROWS
 
-// ------------------
-ctx.resetTransform() // reset transform
-// ------------------
+for (let i = 0; i < COLS; i++) {
+    for (let j = 0; j < ROWS; j++) {
+        const x_ = i * cellWidth + cellWidth / 2
+        const y_ = j * cellHeight + cellHeight / 2
+        const colors_ = colors[(i+j) % colors.length]
+        
+        ctx.save()
+        ctx.translate(x_, y_)
+        if(Math.random() > 0.7) {
+            ctx.rotate(deg2rad(45))
+            ctx.scale(.5, .5)
+        }
+        drawRectangle(cellWidth, cellHeight, colors_)
 
-// 3 - Save & Restore
-ctx.save()
-ctx.translate(600, size / 2) // offset = size/2 sur l'axe Y pour ne pas couper le carré
-drawRectangle(green)
-ctx.restore()
-
-ctx.save()
-ctx.translate(600, 600)
-ctx.rotate(deg2rad(45)) // !! order
-drawRectangle(green)
-ctx.restore()
+        ctx.restore()
+    }
+}
