e95e26cb6522d439776ce0d33e4a4bf6fbbb3677
Adrien Husson <husson.adr@gmail.com>
2024-03-12 16:32:33 +0100

    init

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ const ctx = canvas.getContext('2d') // context type: https://developer.mozilla.o
  *  CONFIG
  */
 
-const ROWS = 6
+const ROWS = 5
 const COLS = 6
 
 const red = '#E83A4E'
@@ -27,37 +27,4 @@ const drawRectangle = (cWidth, cHeight, color) => {
 
 const deg2rad = (deg) => {
     return deg * 2 * Math.PI / 360
-}
-
-/**
- *  MAIN
- */
-
-// Get canvas dimensions
-console.log(canvas.getBoundingClientRect())
-const canvasWidth = canvas.getBoundingClientRect().width
-const canvasHeight = canvas.getBoundingClientRect().height
-canvas.width = canvasWidth
-canvas.height = canvasHeight
-
-// Compute cells dimensions
-const cellWidth = canvasWidth / COLS
-const cellHeight = canvasHeight / ROWS
-
-for (let i = 0; i < COLS; i++) {
-    for (let j = 0; j < ROWS; j++) {
-        const x_ = i * cellWidth + cellWidth / 2
-        const y_ = j * cellHeight + cellHeight / 2
-        const colors_ = colors[(i+j) % colors.length]
-        
-        ctx.save()
-        ctx.translate(x_, y_)
-        if(Math.random() > 0.7) {
-            ctx.rotate(deg2rad(45))
-            ctx.scale(.5, .5)
-        }
-        drawRectangle(cellWidth, cellHeight, colors_)
-
-        ctx.restore()
-    }
 }
