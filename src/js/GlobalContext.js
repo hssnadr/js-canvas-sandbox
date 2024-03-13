513cc8f17a6782b3c41a9f7e8d1fdfcd2f9dc89e
Adrien Husson <husson.adr@gmail.com>
2024-03-13 20:26:55 +0100

    init + destroy

--- src/js/GlobalContext.js
+++ src/js/GlobalContext.js
@@ -1,13 +1,27 @@
 import Debug from "./utils/Debug"
+import Time from "./utils/Time"
 
 let instance = null
 
 export default class GlobalContext {
     constructor() {
-        if(!!instance) return instance
+        if (!!instance) return instance
         instance = this
 
         // Debug
         this.debug = new Debug()
+
+        // Time
+        this.time = new Time()
+        this.time.on('update', () => { this.update() })
+    }
+
+    update() {
+        console.log(this.time.elapsed)
+    }
+
+    destroy() {
+        this.time.off('update')
+        if(this.debug.active) this.debug.ui.destroy()
     }
 }
